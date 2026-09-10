e7e6c3cb8064d4f2e6d87a71b21762554b15dc64
Joshua Mensah <joshmensah18@gmail.com>
2026-09-10 22:16:45 +0000

    feat(auth): initial jwt filter

---
+++ backend/src/main/java/northjosh/auth/config/JwtFilter.java
@@ -0,0 +1,45 @@
+package northjosh.auth.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import northjosh.auth.services.jwt.JwtService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+	private final JwtService jwtService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		String authHeader = request.getHeader("Authorization");
+
+		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+			doFilter(request, response, filterChain);
+		}
+
+		try {
+			String token = authHeader.substring(7);
+			var claims = jwtService.decodeToken(token);
+
+			var auth = new UsernamePasswordAuthenticationToken(claims.getSubject(), null, null);
+			auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+			SecurityContextHolder.getContext().setAuthentication(auth);
+		} catch (Exception e) {
+			SecurityContextHolder.clearContext();
+		}
+
+		filterChain.doFilter(request, response);
+	}
+}
