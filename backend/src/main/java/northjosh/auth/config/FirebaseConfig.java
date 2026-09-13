af0b79e334684cc9cddda958a3848415bd1ea4e0
Joshua Mensah <joshmensah18@gmail.com>
2026-09-13 18:36:28 +0000

    feat(push): add firebase cloud messaging

---
+++ backend/src/main/java/northjosh/auth/config/FirebaseConfig.java
@@ -0,0 +1,42 @@
+package northjosh.auth.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class FirebaseConfig {
+
+	@Bean
+	public FirebaseApp fireBaseApp() {
+
+		try {
+			ResourceLoader resourceLoader = new DefaultResourceLoader();
+			Resource resource = resourceLoader.getResource(
+					"classpath:/auth-levels-northjosh-firebase-adminsdk-fbsvc-a39ca433f6.json");
+
+			FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+
+			FirebaseOptions options = FirebaseOptions.builder()
+					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+					.build();
+			return FirebaseApp.initializeApp(options);
+
+		} catch (FileNotFoundException e) {
+			log.error("Firebase Config Not Found");
+			throw new IllegalStateException("No Firebase Config");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
