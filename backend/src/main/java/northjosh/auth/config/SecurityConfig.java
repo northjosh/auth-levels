e61f6a90fb48bd26768b6c16a9a94996466eeb66
Joshua Mensah <joshmensah18@gmail.com>
2026-09-20 23:03:02 +0000

    fix: restore path
    Signed-off-by: Joshua Mensah <joshmensah18@gmail.com>

--- backend/src/main/java/northjosh/auth/config/SecurityConfig.java
+++ backend/src/main/java/northjosh/auth/config/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig {
 								"/reset-password",
 								"/push/generate",
 								"/push/listen",
+								"/push/sse",
 								"/webauthn/**",
 								"/webauthn/generate",
 								"/devices/pair")
@@ -78,7 +79,7 @@ public class SecurityConfig {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOriginPatterns(List.of("*"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "ngrok-skip-browser-warning"));
 		configuration.setAllowCredentials(true);
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
